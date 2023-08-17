30048912dd15cf6b02cd17dc3b8d5ab1f33a551d
fuadreza <fuadreza1011@gmail.com>
2023-08-17 10:38:23 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Fuad Reza Pahlevi on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
